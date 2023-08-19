65eca449da3c673b5dca38c27c8dd7ef345890ba
Erik Grip <erik.grip@outlook.com>
2023-08-19 11:19:52 +0200

    Format.

--- src/data_downloader/run.py
+++ src/data_downloader/run.py
@@ -1,4 +1,7 @@
-from src.data_downloader.data_downloader import download_zip_file, extract_files_by_extension
+from src.data_downloader.data_downloader import (
+    download_zip_file,
+    extract_files_by_extension,
+)
 
 
 URL = "https://github.com/erikgrip/swedish_parliament_motion_summarization/archive/refs/heads/main.zip"  # pylint: disable=line-too-long
@@ -7,4 +10,4 @@ TARGET_EXTENSIONS = [".md", ".mdx"]
 OUTPUT_PATH = "data/unzipped/"
 
 download_zip_file(URL, SAVE_PATH)
-extract_files_by_extension(SAVE_PATH, TARGET_EXTENSIONS, OUTPUT_PATH )
+extract_files_by_extension(SAVE_PATH, TARGET_EXTENSIONS, OUTPUT_PATH)

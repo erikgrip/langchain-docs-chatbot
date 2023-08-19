4e63aa94892cd060282b3e31af475cad6085eabd
Erik Grip <erik.grip@outlook.com>
2023-08-19 15:53:55 +0200

    Add task for pylint, mypy etc.

---
+++ tasks/lint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -uo pipefail
+set +e
+
+FAILURE=false
+
+echo "safety (failure is tolerated)"
+poetry run pip freeze | poetry run safety check --stdin
+
+echo "pylint"
+pylint src || FAILURE=true
+
+echo "pycodestyle"
+pycodestyle src || FAILURE=true
+
+echo "pydocstyle"
+pydocstyle src|| FAILURE=true
+
+echo "mypy"
+mypy src || FAILURE=true
+
+echo "bandit"
+bandit -ll -r src || FAILURE=true
+
+echo "shellcheck"
+find . -name "*.sh" -print0 | xargs -0 shellcheck || FAILURE=true
+
+if [ "$FAILURE" = true ]; then
+  echo "Linting failed"
+  exit 1
+fi
+echo "Linting passed"
+exit 0

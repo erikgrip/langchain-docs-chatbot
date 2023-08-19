412bf2bbf550f03a97f87ac962cc4bde26661787
Erik Grip <erik.grip@outlook.com>
2023-08-19 11:17:35 +0200

    WIP: Add helper file to run data downloader.

--- src/data_downloader/run.py
+++ src/data_downloader/run.py
@@ -4,7 +4,7 @@ from src.data_downloader.data_downloader import download_zip_file, extract_files
 URL = "https://github.com/erikgrip/swedish_parliament_motion_summarization/archive/refs/heads/main.zip"  # pylint: disable=line-too-long
 SAVE_PATH = "test.zip"
 TARGET_EXTENSIONS = [".md", ".mdx"]
-OUTPUT_PATH = "unzipped/"
+OUTPUT_PATH = "data/unzipped/"
 
 download_zip_file(URL, SAVE_PATH)
 extract_files_by_extension(SAVE_PATH, TARGET_EXTENSIONS, OUTPUT_PATH )

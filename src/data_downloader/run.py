21dbd925ac1dbbeae969add567384772ea78db35
Erik Grip <erik.grip@outlook.com>
2023-08-19 15:40:11 +0200

    Rename downloaded zip file.

--- src/data_downloader/run.py
+++ src/data_downloader/run.py
@@ -5,7 +5,7 @@ from src.data_downloader.data_downloader import (
 
 
 URL = "https://github.com/erikgrip/swedish_parliament_motion_summarization/archive/refs/heads/main.zip"  # pylint: disable=line-too-long
-SAVE_PATH = "test.zip"
+SAVE_PATH = "data/zipped/tmp.zip"
 TARGET_EXTENSIONS = [".md", ".mdx"]
 OUTPUT_PATH = "data/unzipped/"
 

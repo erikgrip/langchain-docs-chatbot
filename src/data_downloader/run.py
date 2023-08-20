d6217626e479e41ef525ccb56ea822d7c5c2e181
Erik Grip <erik.grip@outlook.com>
2023-08-20 08:42:08 +0200

    Use local zip file if exists.

--- src/data_downloader/run.py
+++ src/data_downloader/run.py
@@ -1,3 +1,5 @@
+import os
+
 from src.data_downloader.data_downloader import (
     download_zip_file,
     extract_files_by_extension,
@@ -9,5 +11,10 @@ SAVE_PATH = "data/zipped/tmp.zip"
 TARGET_EXTENSIONS = [".md", ".mdx"]
 OUTPUT_PATH = "data/unzipped/"
 
-download_zip_file(URL, SAVE_PATH)
+# Avoid downloading over and over for now
+if not os.path.exists(SAVE_PATH):
+    print(f"Downloading the repository from {URL}")
+    download_zip_file(URL, SAVE_PATH)
+else:
+    print(f"Using previously downloaded file {SAVE_PATH}")
 extract_files_by_extension(SAVE_PATH, TARGET_EXTENSIONS, OUTPUT_PATH)

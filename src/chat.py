0fbca736dba5c0ea6c7c8d1d1dcca1828481853c
Erik Grip <erik.grip@outlook.com>
2023-10-01 09:42:14 +0200

    Gpt4All lab.

--- src/chat.py
+++ src/chat.py
@@ -1,9 +1,12 @@
 import ssl
 
 import nltk
+from langchain import LLMChain, PromptTemplate
+from langchain.chains import RetrievalQA
 
 from src.data_downloader.run import download_repo
 from src.doc_store.doc_store import DocStore
+from src.model.gpt4all import get_gpt4all_falcon
 
 # HACK: https://github.com/gunthercox/ChatterBot/issues/930#issuecomment-322111087
 try:
@@ -23,3 +26,36 @@ nltk.download("punkt")
 download_repo()
 args = {"hf_repo_id": "sentence-transformers/all-mpnet-base-v2"}
 doc_store = DocStore()
+llm = get_gpt4all_falcon(max_tokens=500, streaming=True, temp=0.1)
+
+
+def dummy_chain():
+    """Answer an example question using a LLM chain."""
+    template = """\
+    Context: Two tomatoes were crossing a road when one of \
+    them suddenly got run over by a truck. Then the other \
+    tomato said: come on ketchup, lets go.
+
+    Question: {question}
+
+    Answer:
+    """
+
+    question = "What is funny about this joke?"
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    llm_chain.run({"question": question})
+
+
+def run_chat(model, document_store):
+    """Answer a question with document store search."""
+    question = "What is the purpose of langchain?"
+    qa_chain = RetrievalQA.from_chain_type(
+        model, retriever=document_store.db.as_retriever()
+    )
+    answer = qa_chain({"query": question})
+    print(answer)
+
+
+# doc_store.db_from_docs_dir("data/unzipped/langchain-master/docs")
+run_chat(llm, doc_store)
--- src/model/gpt4all.py
+++ src/model/gpt4all.py
@@ -16,7 +16,7 @@ def download_model(save_path, url):
                     f.write(chunk)
 
 
-def get_gpt4all():
+def get_gpt4all_falcon(**kwargs):
     """Get GPT4All model."""
     model_file = "ggml-model-gpt4all-falcon-q4_0.bin"
     save_path = f"./data/{model_file}"
@@ -25,4 +25,17 @@ def get_gpt4all():
     )
     download_model(save_path, download_url)
     callbacks = [StreamingStdOutCallbackHandler()]
-    return GPT4All(model=save_path, callbacks=callbacks, verbose=True)
+    return GPT4All(model=save_path, callbacks=callbacks, verbose=True, **kwargs)
+
+
+def get_gpt4all_llama(**kwargs):
+    """Get GPT4All model."""
+    model_file = "llama-2-7b-chat.ggmlv3.q4_0.bin"
+    save_path = f"./data/{model_file}"
+    download_url = (
+        "https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/"
+        + f"resolve/main/{model_file}"
+    )
+    download_model(save_path, download_url)
+    callbacks = [StreamingStdOutCallbackHandler()]
+    return GPT4All(model=save_path, callbacks=callbacks, verbose=True, **kwargs)

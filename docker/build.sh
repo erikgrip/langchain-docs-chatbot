60726703f28085969349f9fd85d8105f3f12d3ef
Erik Grip <erik.grip@outlook.com>
2023-11-26 13:42:09 +0100

    Update build script.

--- docker/build.sh
+++ docker/build.sh
@@ -7,7 +7,7 @@ poetry export -f requirements.txt --output docker/requirements.txt --without-has
 docker  build -t langchain-docs-chatbot -f ./docker/Dockerfile .
 
 # Run docker image and add data/ directory as volume
-docker run --rm -p 8501:8501 -v ./data:/data --env-file .env langchain-docs-chatbot 
+docker run --rm -p 8501:8501 -v ./data:/data --env-file .env langchain-docs-chatbot &&
 
 # Remove dependencies
 rm ./docker/requirements.txt
